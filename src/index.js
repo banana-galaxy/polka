134a1460d651d4c633c896d7e25e223b0331df26
Alex <cawasp@gmail.com>
2024-05-17 20:07:24 -0700

    Initial commit
    Added divs for menu, settings, and game, and did basic menu styling

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.getElementById('settings').style.visibility = 'hidden'
+document.getElementById('game').style.visibility = 'hidden'
+let playing = false
+
+function play() {
+  playing = true
+}
