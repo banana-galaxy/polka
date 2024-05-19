6a2eb6be9ad2596408fe2521a45854f626a5411d
Alex <cawasp@gmail.com>
2024-05-18 21:40:54 -0700

    Basic game mechanics added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 document.getElementById('settings').style.visibility = 'hidden'
 document.getElementById('game').style.visibility = 'hidden'
 let playing = false
+let x = 0
+let y = 0
+let spawn_time = 0
+
+function spawnDot() {
+  x = Math.random() * 851
+  y = Math.random() * 851
+  let dot = document.createElement('img')
+  dot.setAttribute("src", "dot.svg")
+  dot.setAttribute("width", "50")
+  dot.setAttribute("height", "50")
+  dot.style.marginLeft = String(x) + "px"
+  dot.style.marginTop = String(y) + "px"
+  dot.onmouseover = () => {
+    let container = document.getElementById('container')
+    container.innerHTML = ""
+    console.log(Date.now() - spawn_time)
+    setTimeout(() => {
+      spawnDot()
+    }, Math.random() * 2000)
+  }
+  let container = document.getElementById('container')
+  container.appendChild(dot)
+  spawn_time = Date.now()
+}
 
 function play() {
   playing = true
+  document.getElementById('menu').style.visibility = 'hidden'
+  document.getElementById('game').style.visibility = 'visible'
+  spawnDot()
 }
